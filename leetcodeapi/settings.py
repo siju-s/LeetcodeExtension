9189c9af485cf3edda49adaed4f21576e2086ba1
Siju Sakaria <sijus887@gmail.com>
2022-09-11 00:30:42 -0400

    Add allowed hosts to settings.py

--- leetcodeapi/settings.py
+++ leetcodeapi/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sn&vh#)91vb=^%a2104^sw(w0sn(gocs^*s)_ixd53e0zr_=uj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'leetcode-details.herokuapp.com']
 
 # Application definition
 
