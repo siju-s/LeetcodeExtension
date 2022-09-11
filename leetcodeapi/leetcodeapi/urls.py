a7a30da5e832eb24f4b435326f972633e44827b6
Siju Sakaria <sijus887@gmail.com>
2022-09-10 23:40:36 -0400

    [Heroku] Fix for module not found

--- leetcodeapi/leetcodeapi/urls.py
+++ leetcodeapi/leetcodeapi/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from myapi.views import *
+from leetcodeapi.myapi.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
