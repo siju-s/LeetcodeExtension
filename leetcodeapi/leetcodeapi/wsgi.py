f9a044854604612dc88d175b1726f3cddaa520ee
Siju Sakaria <sijus887@gmail.com>
2022-09-10 23:36:10 -0400

    Fix leetcodeapi.settings not found on Heroku

--- leetcodeapi/leetcodeapi/wsgi.py
+++ leetcodeapi/leetcodeapi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leetcodeapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leetcodeapi.leetcodeapi.settings')
 
 application = get_wsgi_application()
