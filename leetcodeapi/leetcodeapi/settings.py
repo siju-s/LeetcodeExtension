bb29509343b3ebbdd412835a1782ad1910ae73eb
Siju Sakaria <sijus887@gmail.com>
2022-09-10 23:54:24 -0400

    [Heroku] Possible fix for H10 error

--- leetcodeapi/leetcodeapi/settings.py
+++ leetcodeapi/leetcodeapi/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'leetcodeapi.myapi',
+    'myapi',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- leetcodeapi/leetcodeapi/urls.py
+++ leetcodeapi/leetcodeapi/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from leetcodeapi.myapi.views import *
+from myapi.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
